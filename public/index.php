1df34e1232ac285f50b4d43a265c8d55d7b91921
Matthew Willems <matthewwillems2003@outlook.com>
2023-11-01 16:40:20 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+
+    require("../views/layout/layout.php"); // Bestand vereist voordat het kan worden uitgevoerd. Geeft een error in plaats van een waarschuwing, als het mist.
+    
+    ?>
+</body>
+</html>
---
+++ views/layout/layout.php
@@ -0,0 +1,5 @@
+<article>
+    <h1>Product</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam debitis corrupti reprehenderit? Adipisci saepe, tenetur labore veritatis eius magni ab assumenda perferendis necessitatibus deserunt! Iusto consectetur fuga dolore voluptatibus aliquid!</p>
+    <img src="" alt="">
+</article>
